88e9173d7eb85b435222a3e4ef275852600cf149
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2023-07-06 17:07:49 +0530

    string comparison

---
+++ src/main/kotlin/StringEquality.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val name1 = "Saleh"
+    val name2 = "Jamila"
+    val name3 = String("Saleh".toCharArray())
+
+    println(" ${name1===name3}")
+    println("${ name1.equals(name3) }")
+}
