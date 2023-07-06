8ebf56381cc3c2807d745f92f1e34d8491ee9662
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2023-07-06 13:09:40 +0530

    data types

---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var n: Int = 19
+    var l:Long = 100L
+    var d:Double = 3.14
+    var f:Float = 3.14F
+    var b:Boolean = true
+    var s:String = "Amigoscode"
+    var c:Char = 'a'
+}
