a79474614632326afc60b1a2d5be8a27cb0443de
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2023-07-06 15:57:15 +0530

    string

---
+++ src/main/kotlin/Strings.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val name = "Ram"
+    println(name)
+    println(name.uppercase())
+    println(name.lowercase())
+    println(name.length)
+    println(name[0])
+
+    val age = 20
+    val msg = "$name $age"
+
+    println(msg)
+
+    v
+}
