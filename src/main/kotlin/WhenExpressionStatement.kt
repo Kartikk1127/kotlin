232067bd7fa8fd6ec8884a8b7d74fd9fd6b1335e
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2023-07-10 11:39:47 +0530

    more use cases

--- src/main/kotlin/WhenExpressionStatement.kt
+++ src/main/kotlin/WhenExpressionStatement.kt
@@ -12,15 +12,27 @@
 //}
 
 //when as an expression
+//fun main() {
+//    val gender = "M"
+//    val value = when(gender){
+//        "F"-> "female"
+//        "M"-> "male"
+//        else -> {
+//            "Unknown gender"
+//        }
+//    }
+//
+//    println(value)
+//}
+
+//more use cases of when statement
 fun main() {
-    val gender = "M"
-    val value = when(gender){
-        "F"-> "female"
-        "M"-> "male"
+    val age = 4
+    when(age){
+        in 13..19 -> println("teenage")
+        in 0..12 -> println("below 12")
         else -> {
-            "Unknown gender"
+            println("an adult")
         }
     }
-
-    println(value)
 }
