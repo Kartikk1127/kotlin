6486195073506c411d654c5f934c252c353f7233
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2023-07-11 16:30:30 +0530

    classes

---
+++ src/main/kotlin/Person.kt
@@ -0,0 +1,14 @@
+class Person(_firstName: String, _lastName: String)
+//if you do not have any properties or arguments in your primary constructor then you don't need a constructor
+{
+    val firstName: String
+    val lastName: String
+
+    init {
+        firstName=_firstName
+        lastName=_lastName
+    }
+}
+fun main() {
+    val person = Person("Kartik", "Srivastava") //creating an instance of the Person class
+}
