b1582252746f44135c7e95d9789f6f8fbaf2db4c
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2023-07-12 15:25:07 +0530

    custom getters, setters

--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -1,18 +1,21 @@
 class Person(val firstName: String="Kartikey", val lastName: String = "Srivastava")
 //if you do not have any properties or arguments in your primary constructor then you don't need a constructor
 {
-//    init {
-//        println("Init 1")
-//    }
-//    constructor(): this("Peter","Parker"){
-//        println("This is secondary constructor")
-//    }
-//    init {
-//        println("init 2")
-//    }
+var nickName: String? = null
+    set(value) {
+        field = value
+        println("the new nickname is $value")
+    }
+    get(){
+        println("the returned value is $field")
+        return field
+    }
 }
 fun main() {
     val person = Person() //creating an instance of the Person class
     person.lastName
     person.firstName
+    person.nickName="Kartik"
+    person.nickName = "New nickname"
+    println(person.nickName)
 }
