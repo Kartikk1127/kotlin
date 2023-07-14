7662ff4a90fb079b54edd9a03202ee82f6c4db9e
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2023-07-14 11:15:05 +0530

    type casting

--- src/main/kotlin/PersonInfoProvider.kt
+++ src/main/kotlin/PersonInfoProvider.kt
@@ -33,4 +33,18 @@ fun main() {
     val provider = BasicInfoProvider()
     provider.printInfo(Person())
     provider.getSessionId()
+
+    checkTypes(provider)
+}
+
+fun checkTypes(infoProvider: PersonInfoProvider){
+    if(infoProvider !is SessionInfoProvider){
+        println("not a session info provider")
+    }
+    else
+    {
+        println("is a session info provider")
+//        (infoProvider as SessionInfoProvider).getSessionId()
+        infoProvider.getSessionId()
+    }
 }
