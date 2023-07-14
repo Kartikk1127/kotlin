d2e387bf6dfe810cfdc82510475dbb01f8534fef
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2023-07-14 12:07:08 +0530

    inheritance

--- src/main/kotlin/PersonInfoProvider.kt
+++ src/main/kotlin/PersonInfoProvider.kt
@@ -11,7 +11,7 @@ interface SessionInfoProvider{
 }
 
 //here class implements the interface
-class BasicInfoProvider : PersonInfoProvider, SessionInfoProvider {
+open class BasicInfoProvider : PersonInfoProvider, SessionInfoProvider {
     //    override fun printInfo(person: Person) {
 //        println("basicInfoProvider")
 //        person.printInfo()
@@ -19,18 +19,21 @@ class BasicInfoProvider : PersonInfoProvider, SessionInfoProvider {
     override val providerInfo: String
         get() = "BasicInfoProvider"
 
+   protected open val sessionIdPrefix = "Session"
+
     override fun printInfo(person: Person) {
         super.printInfo(person)
         println("additional print statement")
     }
 
     override fun getSessionId(): String {
-        return "Session"
+        return sessionIdPrefix
     }
 }
 
 fun main() {
-    val provider = BasicInfoProvider()
+    val provider = FancyInfoProvider()
+
     provider.printInfo(Person())
     provider.getSessionId()
 
